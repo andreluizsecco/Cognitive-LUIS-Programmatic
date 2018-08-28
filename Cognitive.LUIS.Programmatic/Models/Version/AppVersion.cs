7dc782a01acf62389d55ebf72f4038693a91f2ba
Michel Weber <michel.weber@sbb.ch>
2018-08-28 14:24:51 +0200

    Support getting all versions & only a specific version (covered by tests)

---
+++ Cognitive.LUIS.Programmatic/Models/Version/AppVersion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cognitive.LUIS.Programmatic.Models
+{
+    public class AppVersion
+    {
+        public string Version { get; set; }
+
+        public DateTime CreatedDateTime { get; set; }
+        
+        public DateTime LastModifiedDateTime { get; set; }
+        
+        public DateTime? LastTrainedDateTime { get; set; }
+        
+        public DateTime? LastPublishedDateTime { get; set; }
+        
+        public string EndpointUrl { get; set; }
+        
+        public AssignedEndpointKey AssignedEndpointKey { get; set; }
+        
+        // public ExternalApiKeys externalApiKeys { get; set; }
+        
+        public int IntentsCount { get; set; }
+        
+        public int EntitiesCount { get; set; }
+        
+        public int EndpointHitsCount { get; set; }
+        
+        /// <summary>
+        /// "NeedsTraining", "InProgress", "Trained"
+        /// </summary>
+        public string TrainingStatus { get; set; }
+    }
+}
---
+++ Cognitive.LUIS.Programmatic/Models/Version/AssignedEndpointKey.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cognitive.LUIS.Programmatic.Models
+{
+    public class AssignedEndpointKey
+    {
+        public string SubscriptionKey { get; set; }
+
+        public string SubscriptionRegion { get; set; }
+
+        public string SubscriptionName { get; set; }
+    }
+}
