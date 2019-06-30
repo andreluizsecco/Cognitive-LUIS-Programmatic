766a1bd06719d9642dc614ec7be75b1ac6c0536a
André Secco <andreluizsecco@yahoo.com.br>
2019-06-29 23:08:15 -0300

    Updated publish model

--- Cognitive.LUIS.Programmatic.Tests/PublishTests.cs
+++ Cognitive.LUIS.Programmatic.Tests/PublishTests.cs
@@ -26,7 +26,7 @@ namespace Cognitive.LUIS.Programmatic.Tests
                 var trainingDetails = await client.Training.TrainAndGetFinalStatusAsync(appId, appVersion);
                 if (trainingDetails.Status.Equals("Success"))
                 {
-                    var publish = await client.Publishing.PublishAsync(appId, appVersion, false, BaseTest.Region.ToString().ToLower());
+                    var publish = await client.Publishing.PublishAsync(appId, appVersion);
                     Assert.NotNull(publish);
                 }
             }
@@ -38,7 +38,7 @@ namespace Cognitive.LUIS.Programmatic.Tests
             using (var client = new LuisProgClient(SubscriptionKey, Region))
             {
                 var ex = await Assert.ThrowsAsync<Exception>(() =>
-                    client.Publishing.PublishAsync(InvalidId, appVersion, false, "westus"));
+                    client.Publishing.PublishAsync(InvalidId, appVersion));
 
                 Assert.Equal("BadArgument - Cannot find an application with the ID 51593248-363e-4a08-b946-2061964dc690.", ex.Message);
             }
